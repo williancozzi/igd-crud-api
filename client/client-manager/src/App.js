7c231eedce54c223a11a6d0eab18220073c50619
williancozzi <williancozzi@gmail.com>
2021-01-07 21:14:55 -0300

    added get all function

--- client/client-manager/src/App.js
+++ client/client-manager/src/App.js
@@ -1,23 +1,39 @@
 import './App.css';
 import axios from "axios";
+import React from 'react';
 
 const api = axios.create({
     baseURL: `http://localhost:3000`
 });
 
-const handleSubmit = async () => {
-    const { response } = await api.get("/users");
-
-    console.log({ response });
-    console.log("handle");
-
-};
-
 function App() {
+
+    const [users, setUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchUser = async () => {
+            console.log('entrei');
+            const { data } = await api.get('/users');
+            setUsers(data);
+        }
+
+        fetchUser();
+    }, []);
+
+    console.log('resultado', users);
+
     return (
         <div>
             <h1 className="center">Controle de usuários - IGD</h1>
-            <button onClick={handleSubmit}>get all</button>
+            {users.map((user => {
+                return (
+                    <ul key={user.id}>
+                        <li>{user.name}</li>
+                        <li>{user.email}</li>
+                        <li>{user.tags}</li>
+                    </ul>
+                )
+            }))}
         </div>
     );
 }
--- client/client-manager/src/http-common.js
+++ client/client-manager/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Defines the URL base for use of the service
 export default axios.create({
-    baseURL: "http://localhost:3001/",
+    baseURL: "http://localhost:3000/",
     headers: {
         "Content-type": "application/json",
     },
